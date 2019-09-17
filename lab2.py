ccc0ac5465c42c9fbfb47c3046cfce4e7cd4ae7a
Deathstar-git <79025632112tch@gmail.com>
2019-09-18 00:55:51 +0800

    Finish working version with interface

--- lab2.py
+++ lab2.py
@@ -21,21 +21,31 @@ def get_dict():
         for i in firm[j]:
             firm[j][i] = randint(10000, 100000)  # рандомные значения для firm
 
-    print("ВСЕ ПОКАЗАТЕЛИ ФИРМЫ:\n")
-    for j in firm:
-        print(str(j) + ":" + str(firm[j]))  # вывод показателей фирмы
+    print("БАЗА ДАННЫХ ФИРМЫ")
+    print("Выберите действие:\n" + "1:Показать базу данных\n" +
+          "2:Посчитать средние значения показателей")
+    choose = input("Введите 1 или 2:")
 
-    print("")  # отступ
+    while choose != '1' and choose != '2':  # Проверка на правильность выбранного действия
+        choose = input("Введите 1 или 2:")
+
+    if choose == '1':  # действие 1
+        print("ВСЕ ПОКАЗАТЕЛИ ФИРМЫ:\n")
+        for j in firm:
+            print(str(j) + ":" + str(firm[j]))  # вывод показателей фирмы
+
+        print("")  # отступ
 
     res = [0]*5
     for j in firm:
         val = list(firm[j].values())
         for i in range(len(val)):
             res[i] += val[i]
-    print("CРЕДНИЕ ЗНАЧЕНИЯ ПОКАЗАТЕЛЕЙ ФИРМЫ:\n")
-    for i in range(len(res)):  # цикл для вывода средних значений показателей firm
-        res[i] /= len(firm)
-        print("Cреднее значение '" + list(firm['br1'].keys())[i] + "' показателя по всей фирме:" + str(res[i]))
+    if choose == "2":  # действие 2
+        print("CРЕДНИЕ ЗНАЧЕНИЯ ПОКАЗАТЕЛЕЙ ФИРМЫ:\n")
+        for i in range(len(res)):  # цикл для вывода средних значений показателей firm
+            res[i] /= len(firm)
+            print("Cреднее значение '" + list(firm['br1'].keys())[i] + "' показателя по всей фирме:" + str(res[i]))
 
 
 get_dict()
