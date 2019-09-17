79941932ee1071847512c68838d043c86b48dd19
Deathstar-git <79025632112tch@gmail.com>
2019-09-18 00:15:10 +0800

    Finish working version 2.0

--- lab2.py
+++ lab2.py
@@ -19,17 +19,21 @@ def get_dict():
             'br10': {'revenue': (), 'costs': (), 'debt': (), 'stocks': (), 'donations': ()}, }
     for j in firm:
         for i in firm[j]:
-            firm[j][i] = randint(10000, 100000)
+            firm[j][i] = randint(10000, 100000)  # рандомные значения для firm
 
-    print(firm)
+    print("ВСЕ ПОКАЗАТЕЛИ ФИРМЫ:\n")
+    for j in firm:
+        print(str(j) + ":" + str(firm[j]))  # вывод показателей фирмы
+
+    print("")  # отступ
+
     res = [0]*5
     for j in firm:
-
         val = list(firm[j].values())
         for i in range(len(val)):
             res[i] += val[i]
-
-    for i in range(len(res)):
+    print("CРЕДНИЕ ЗНАЧЕНИЯ ПОКАЗАТЕЛЕЙ ФИРМЫ:\n")
+    for i in range(len(res)):  # цикл для вывода средних значений показателей firm
         res[i] /= len(firm)
         print("Cреднее значение '" + list(firm['br1'].keys())[i] + "' показателя по всей фирме:" + str(res[i]))
 
